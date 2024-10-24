7de84db4ef2957e21b7947e62d58e77b4e1d1ba0
Felipe Campos <felipecamposdev@gmail.com>
2024-10-24 10:41:40 -0300

    update links

--- src/app/SocialLinks.tsx
+++ src/app/SocialLinks.tsx
@@ -7,25 +7,25 @@ const SocialLinks = () => {
   return (
     <ul className="flex justify-center gap-3 mb-12">
       <li>
-        <Link href="#" className="text-2xl block p-2 transition-all hover:scale-125" title="Github">
+        <Link href="https://github.com/camposdev" target="_blank" className="text-2xl block p-2 transition-all hover:scale-125" title="Github">
           <FaGithub />
         </Link>
       </li>
 
       <li>
-        <Link href="#" className="text-2xl block p-2 transition-all hover:scale-125" title="LinkedIn">
+        <Link href="https://www.linkedin.com/in/felipefcampos" target="_blank" className="text-2xl block p-2 transition-all hover:scale-125" title="LinkedIn">
           <FaLinkedinIn />
         </Link>
       </li>
 
       <li>
-        <Link href="#" className="text-2xl block p-2 transition-all hover:scale-125" title="WhatsApp">
+        <Link href="https://wa.me/5548996623917" target="_blank" className="text-2xl block p-2 transition-all hover:scale-125" title="WhatsApp">
           <FaWhatsapp />
         </Link>
       </li>
 
       <li>
-        <Link href="#" className="text-2xl block p-2 transition-all hover:scale-125" title="Download CV">
+        <Link href="/cv-felipe-campos-en.pdf" download target="_blank" className="text-2xl block p-2 transition-all hover:scale-125" title="Download CV">
           <TbFileCv />
         </Link>
       </li>
