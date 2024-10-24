eb7617411f2419b726b78396d52cb11868ab53ba
Felipe Campos <felipecamposdev@gmail.com>
2024-10-24 02:34:59 -0300

    fix response

--- src/app/Contact.tsx
+++ src/app/Contact.tsx
@@ -8,10 +8,10 @@ const Contact = () => {
       <h3 className="subtitle">Contact me</h3>
 
       <div className='mt-10'>
-        <div className='flex items-center justify-center mb-10 text-slate-400 gap-6'>
+        <div className='flex flex-col gap-2 md:flex-row md:gap-6 items-center justify-center mb-10 text-slate-400'>
           <a href="tel:+5548996623917" target="_blank" className="hover:text-slate-300">+55 (48) 99662-3917</a>
           <span>
-            <GiCoffeeCup className='text-slate-600' />
+            <GiCoffeeCup className='text-slate-600 text-xl' />
           </span>
           <a href="mailto:felipecamposdev@gmail.com" target="_blank" className="hover:text-slate-300">felipecamposdev@gmail.com</a>
         </div>
